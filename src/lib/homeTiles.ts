bb7faf90c4a11e35fe85e8bde6ddf8568b456213
dbailey323 <159084521+dbailey323@users.noreply.github.com>
2025-09-21 14:41:51 +0100

    Update homeTiles.ts

--- src/lib/homeTiles.ts
+++ src/lib/homeTiles.ts
@@ -9,16 +9,16 @@ export type HomeTile = {
 
 export const HOME_TILES: HomeTile[] = [
   { title: "Header", href: "/about", img: "/images/Header.png", alt: "Eir & Wild banner", banner: true },
-  { title: "About Me", href: "/about", img: "/images/AboutMe.png", alt: "About Lara" },
-  { title: "Our Story", href: "/ourstory", img: "/images/OurStory.png", alt: "Our Story" },
+  { title: "About Me", href: "/about", img: "/images/About Me.png", alt: "About Lara" },
+  { title: "Our Story", href: "/ourstory", img: "/images/Our Story.png", alt: "Our Story" },
 
   { title: "Motherhood Classes", href: "/timetable?tab=motherhood", img: "/images/Motherhood.png", alt: "Motherhood classes" },
   { title: "Womanhood Classes", href: "/timetable?tab=womanhood", img: "/images/Womanhood.png", alt: "Womanhood classes" },
-  { title: "Baby Massage", href: "/baby-massage", img: "/images/BabyMassage.png", alt: "Baby massage" },
+  { title: "Baby Massage", href: "/baby-massage", img: "/images/Baby Massage.png", alt: "Baby massage" },
 
   { title: "Personal Training", href: "/personal-training", img: "/images/PT.png", alt: "Personal training" },
-  { title: "Online Classes", href: "/online-classes", img: "/images/OnlineClasses.png", alt: "Online classes" },
-  { title: "One Off Classes", href: "/one-off-classes", img: "/images/OneOff.png", alt: "One-off classes" },
+  { title: "Online Classes", href: "/online-classes", img: "/images/Online Classes.png", alt: "Online classes" },
+  { title: "One Off Classes", href: "/one-off-classes", img: "/images/One Off.png", alt: "One-off classes" },
 
   { title: "Doula Services", href: "/doula", img: "/images/Doula.png", alt: "Doula services" },
   { title: "Reviews", href: "/reviews", img: "/images/Reviews.png", alt: "Customer reviews" },
